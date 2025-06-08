fc9760bb815409e55acfdc59b80b30fd65cf8e2b
WonBin <tray024400@gmail.com>
2025-06-08 12:03:51 +0900

    k8s-hello(Spring)
    Spring Boot 프로젝트 폴더 (기본적인 HTML 출력)

---
+++ k8s-hello/src/main/java/com/example/k8s_hello/K8sHelloApplication.java
@@ -0,0 +1,14 @@
+package com.example.k8s_hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class K8sHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sHelloApplication.class, args);
+	}
+
+}
---
+++ k8s-hello/src/test/java/com/example/k8s_hello/K8sHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k8s_hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
